6135e7f64dfbd36f613ca8015493c3fd1f3641b8
CookiesWithMoloko <95980525+CookiesWithMoloko@users.noreply.github.com>
2022-05-31 00:16:19 +0300

    minor fixes

--- static/js/admin_parsers.js
+++ static/js/admin_parsers.js
@@ -3,7 +3,16 @@ $(function(){
         $('#header').toggleClass('show-menu');
         $('.menu').first().toggleClass('show-menu');
     });
-    $('.last_use-field').forEach((e) => {
-        e.innerText = new Date(parseInt(e.innerText)*1000).toUTCString();
-    })
+    $('.last_use-field').map((index,e) => {
+        let d = new Date(parseInt(e.innerText)*1000);
+        e.innerText =
+            d.getHours() + ":" +
+            d.getMinutes() + " " +
+            d.getDate()  + "." +
+            (d.getMonth()+1) + "." +
+            d.getFullYear();
+
+    });
+
+
 });
