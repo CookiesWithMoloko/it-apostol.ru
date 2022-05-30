3bfbd46bfd067873cc01b600381672f033f4bf0a
CookiesWithMoloko <95980525+CookiesWithMoloko@users.noreply.github.com>
2022-05-31 00:11:33 +0300

    minor fixes

--- static/js/admin_parsers.js
+++ static/js/admin_parsers.js
@@ -3,4 +3,7 @@ $(function(){
         $('#header').toggleClass('show-menu');
         $('.menu').first().toggleClass('show-menu');
     });
+    $('.last_use-field').forEach((e) => {
+        e.innerText = new Date(parseInt(e.innerText)*1000).toUTCString();
+    })
 });
